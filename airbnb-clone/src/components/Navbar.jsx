926b78558bb0c5d3400a5d38a4899d2d41f8b685
unknown <shamy_pw4@hotmail.co.uk>
2023-10-02 16:57:47 +0100

    created navbar with logo

---
+++ airbnb-clone/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+
+import Logo from "../images/airbnb-logo.png";
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src={Logo} className="nav--logo"/>
+        </nav>
+    )
+}
